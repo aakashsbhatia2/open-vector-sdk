47de43c2584478a541a814a7cc944f95a98f9bf9
Aakash Bhatia <aakashsbhatia@outlook.com>
2025-11-09 22:25:50 -0500

    create vector in pgvector

--- pgVector/pgVectorUtils.js
+++ pgVector/pgVectorUtils.js
@@ -1,3 +1,38 @@
-module.exports = {
+const Client = require('pg').Client;
 
+const pgVectorValidationUtils = require('./pgVectorValidationUtils');
+
+async function createCollection(config, options) {
+    try {
+        pgVectorValidationUtils.validatePgVectorConfig(config);
+        const { host, port, user, password, database } = config;
+
+        pgVectorValidationUtils.validatePgVectorCreateCollectionsOptions(options);
+        const client = new Client({
+            user,
+            password,
+            host,
+            port,
+            database,
+        });
+
+        await client.connect();
+        await client.query(`CREATE EXTENSION IF NOT EXISTS vector;`);
+        await client.query(`
+                CREATE TABLE IF NOT EXISTS ${options.collectionName} (
+                id bigserial PRIMARY KEY,
+                embedding vector(${options.vectorSize}),
+                payload jsonb
+            );
+        `);
+
+        await client.end();
+        return;
+    } catch(err) {
+        throw err;
+    }
+}
+
+module.exports = {
+    createCollection
 }
--- pgVector/pgVectorValidationUtils.js
+++ pgVector/pgVectorValidationUtils.js
@@ -1,3 +1,5 @@
+const typeUtils = require('../commonUtils/typeUtils');
+
 function validatePgVectorConfig(config) {
     if (!typeUtils.isNonEmptyString(config?.host)) throw 'Invalid or missing host in PG Vector config';
     if (!typeUtils.isPositiveInteger(config?.port)) throw 'Invalid or missing port in PG Vector config';
@@ -6,6 +8,17 @@ function validatePgVectorConfig(config) {
     if (!typeUtils.isNonEmptyString(config?.database)) throw 'Invalid or missing database name in PG Vector config';
 }
 
+function validatePgVectorCreateCollectionsOptions(options) {
+    if (!typeUtils.isNonEmptyString(options?.collectionName)) {
+        throw 'Invalid or missing collectionName in qdrant collection options';
+    }
+
+    if (!typeUtils.isPositiveInteger(options?.vectorSize)) {
+        throw 'Invalid or missing vectorSize in qdrant collection options';
+    }
+}
+
 module.exports = {
-    validatePgVectorConfig
+    validatePgVectorConfig,
+    validatePgVectorCreateCollectionsOptions
 }
